349103115668c310f8ad612362ccc9d0ec031335
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-10-02 08:56:17 +0500

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { HeaderItemComponent } from './header/header-item/header-item.component';
+import { FormsModule } from '@angular/forms';
+import { ItemComponent } from './header/header-item/item/item.component';
+import { FilterComponent } from './header/header-item/filter/filter.component';
+import { SearchComponent } from './header/header-item/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, TopHeaderComponent
+    AppComponent, HeaderComponent, TopHeaderComponent, HeaderItemComponent, ItemComponent, FilterComponent, SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-top-header',
+  selector: 'top-header',
   templateUrl: './top-header.component.html',
   styleUrls: ['./top-header.component.scss']
 })
-export class TopHeaderComponent {
 
+export class TopHeaderComponent {
+  buttonName: string = "click me"
+  buttonType: string = 'button'
+  imageUrl: string = '../../assets/images/union.png'
+  hiddenText: string = ""
+  inputVal: string = ''
+  count: number = 0
+
+  showText() {
+    this.hiddenText = 'Hey you see me'
+  }
+
+  incrementVal() {
+    this.count++
+  }
+  decrementVal() {
+    this.count--
+  }
 }
